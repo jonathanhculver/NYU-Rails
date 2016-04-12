ca3b219e4b613f3a06e24dbd9f0e8602c3f77c85
Jon Culver <jon.culver@blueapron.com>
2016-04-11 21:09:42 -0400

    Image upload stuff

--- Listings/app/controllers/pictures_controller.rb
+++ Listings/app/controllers/pictures_controller.rb
@@ -10,6 +10,23 @@ class PicturesController < ApplicationController
   end
 
   def create
+    listing_id = params[:listing_id]
+    @listing = Listing.find(listing_id)
+
+    @picture = Picture.new(picture_params)
+    @picture.listing = @listing
+    uploadIO = params[:picture][:image]
+    if uploadIO != nil
+      t = Time.now
+      ext = File.extname(uploadIO.original_filename)
+      tmpFilename = "file_#{t.strftime("%Y%m%d%H%M")}" + ext
+      @picture.filename = tmpFilename
+      filename = Rails.root.join('public','images', tmpFilename)
+      File.open(filename, 'wb') do |file|
+        file.write(uploadIO.read)
+      end
+    end
+
   end
 
   def edit
@@ -22,4 +39,10 @@ class PicturesController < ApplicationController
 
   def destroy
   end
+
+  private
+
+    def picture_params
+      params.require(:picture).permit(:title)
+    end
 end
